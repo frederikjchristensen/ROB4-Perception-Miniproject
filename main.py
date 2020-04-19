8eba54e80f51b6e43383c9dacb689b89fbbcaf2f
frederikjchristensen <frederikjchristensen@gmail.com>
2020-04-19 13:37:01 +0200

    Update 1#

--- main.py
+++ main.py
@@ -10,13 +10,29 @@ import cv2
 import numpy as np
 
 #Import image
-gray = cv2.imread('IMG_20200419_114116.jpg', cv2.IMREAD_GRAYSCALE)
-colour = cv2.imread('IMG_20200419_114116.jpg', cv2.IMREAD_UNCHANGED)
+gray = cv2.imread('IMG_20200419_125217__01.jpg', cv2.IMREAD_GRAYSCALE)
+colour = cv2.imread('IMG_20200419_125217__01.jpg', cv2.IMREAD_UNCHANGED)
+mask = cv2.imread('IMG_20200419_125217__01.jpg', cv2.IMREAD_UNCHANGED)
+
+
+blur = cv2.medianBlur(gray,19)
+th3 = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
 
 ret,thresh = cv2.threshold(gray,127,255,0)
 contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-#cv2.drawContours(colour, contours, -1, (0,0,255), cv2.FILLED)
+cv2.drawContours(colour, contours, -1, (0,0,255), cv2.FILLED)
+cv2.drawContours(mask, contours, -1, (0,0,255), cv2.FILLED)
+contour_mask = cv2.inRange(mask, (0, 0, 255), (0, 0, 255))
+
+kernel_small = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6, 6))
+kernel_large = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (25, 25))
+
+#Apply Opening (Noise removal)
+erosion = cv2.erode(contour_mask, kernel_small, 1)
+dilation = cv2.dilate(erosion, kernel_large, 1)
+
 
 
 
@@ -27,6 +43,11 @@ cv2.namedWindow("Colour", cv2.WINDOW_NORMAL)        # Create window with freedom
 cv2.resizeWindow("Colour", gray.shape[0], gray.shape[1])    
 cv2.imshow("Grayscale", gray)
 cv2.imshow("Colour", colour)
+
+cv2.namedWindow("test", cv2.WINDOW_NORMAL)        # Create window with freedom of dimensions    
+cv2.resizeWindow("test", gray.shape[0], gray.shape[1])  
+cv2.imshow("test", dilation)
+
 
 
 
